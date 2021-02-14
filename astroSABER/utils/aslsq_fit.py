b8dc6a291a2b451381ea9f6332dc9c849039bf41
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-02-14 01:37:03 +0100

    Add files via upload

---
+++ astroSABER/utils/aslsq_fit.py
@@ -0,0 +1,38 @@
+'''asymmetric least squares fit'''
+
+import os
+import numpy as np
+
+from astropy.io import fits
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+
+from tqdm import trange
+import warnings
+
+
+#Asymmetric least squares baseline fit from Eilers et al. 2005
+def baseline_als(y, lam, p, niter):
+    L = len(y)
+    D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+    w = np.ones(L)
+    for i in range(niter):
+        W = sparse.spdiags(w, 0, L, L)
+        Z = W + lam * D.dot(D.transpose())
+        z = spsolve(Z, w*y)
+        w = p * (y > z) + (1-p) * (y < z)
+    return z
+
+#optimized version; this should be faster by a factor ~1.5
+def baseline_als_optimized(y, lam, p, niter):
+    L = len(y)
+    D = sparse.diags([1,-2,1],[0,-1,-2], shape=(L,L-2))
+    D = lam * D.dot(D.transpose()) # Precompute this term since it does not depend on `w`
+    w = np.ones(L)
+    W = sparse.spdiags(w, 0, L, L)
+    for i in range(niter):
+        W.setdiag(w) # Do not create a new matrix, just update diagonal values
+        Z = W + D
+        z = spsolve(Z, w*y)
+        w = p * (y > z) + (1-p) * (y < z)
+    return z
---
+++ astroSABER/utils/aslsq_helper.py
@@ -0,0 +1,93 @@
+'''helper functions'''
+
+import os
+import numpy as np
+
+from astropy.io import fits
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+
+from tqdm import trange
+import warnings
+
+
+def velocity_axes(name):
+	header = fits.getheader(name)
+	n = header['NAXIS3'] #number of channels on spectral axis
+	velocity = (header['CRVAL3'] - header['CRPIX3'] * header['CDELT3']) + (np.arange(n)+1) * header['CDELT3']
+	velocity = velocity / 1000
+	return velocity
+
+
+#taken from Lindner (2014) & Riener (2019); GaussPy(+)
+def count_ones_in_row(data):
+    """ Counts number of continuous trailing '1's
+         Used in the convergence criteria
+    """
+    output = np.zeros(len(data))
+    for i in range(len(output)):
+        if data[i] == 0:
+            output[i] = 0
+        else:
+            total = 1
+            current = 1
+            counter = 1
+            while data[i-counter] == 1:
+                total += 1
+                if i - counter < 0:
+                    break
+                current = data[i - counter]
+                counter += 1
+            output[i] = total
+    return output
+
+
+#simple check
+def check_signal(spectrum, sigma, noise):
+    return np.any(spectrum > sigma * noise)
+            
+
+#check if there is signal in at least xy neighboring channels corresponding to velo_range [km/s]: default 5 channels
+def check_signal_ranges(spectrum, header, sigma=None, noise=None, velo_range=None):
+    vdelt = header['CDELT3'] / 1000.
+    if sigma is None:
+        sigma = 5
+    if noise is None:
+        noise = np.round(np.nanstd(np.append(spectrum[0:int(10*vdelt)],spectrum[int(-10*vdelt):-1])),decimals=2)
+    if np.any(spectrum > sigma * noise):
+        if velo_range is not None:
+            min_channels = velo_range//vdelt
+        else:
+            min_channels = 10
+        channel_logic = np.array([])
+        for i in range(len(spectrum)):
+            channel_test = np.all(spectrum[i] > sigma*noise) 
+            channel_logic = np.append(channel_logic,channel_test)
+        c = count_ones_in_row(channel_logic)
+        return np.any(c > min_channels)
+    else:
+        return False
+
+
+def md_header_2d(fitsfile):
+    header_2d = fits.getheader(fitsfile)
+    del header_2d['NAXIS3']
+    del header_2d['CRPIX3']
+    del header_2d['CDELT3']
+    del header_2d['CUNIT3']
+    del header_2d['CTYPE3']
+    del header_2d['CRVAL3']
+
+    header_2d['NAXIS'] = 2
+    header_2d['WCSAXES'] = 2
+    return header_2d
+
+
+class StopIteration(UserWarning):
+    pass
+
+
+def say(message, verbose=True, end=None):
+    """Diagnostic messages."""
+    if verbose:
+        print(message, end=end)
