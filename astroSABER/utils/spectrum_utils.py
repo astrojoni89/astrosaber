6d8f3dc234adc03ac43679b447f2de98afa9525c
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-03-02 17:01:32 +0100

    Update spectrum_utils.py

--- astroSABER/utils/spectrum_utils.py
+++ astroSABER/utils/spectrum_utils.py
@@ -17,12 +17,40 @@ def pixel_circle_calculation(fitsfile,glon,glat,r):
     delta = abs(header['CDELT1']) #in degree
     pixel_array = []
     if header['NAXIS']==3:
-        central_px = w.all_world2pix(glon,glat,0,1)
+        central_px = w.all_world2pix(Glon,Glat,0,1)
     elif header['NAXIS']==2:
-        central_px = w.all_world2pix(glon,glat,1)
+        central_px = w.all_world2pix(Glon,Glat,1)
     else:
         raise Exception('Something wrong with the header!')
     central_px = [int(np.round(central_px[0],decimals=0))-1,int(np.round(central_px[1],decimals=0))-1]
+    if r is not 'single':
+        circle_size_px = 2*r/3600. / delta
+        circle_size_px = int(round(circle_size_px))
+        px_start = [central_px[0]-circle_size_px/2,central_px[1]-circle_size_px/2]
+        px_end = [central_px[0]+circle_size_px/2,central_px[1]+circle_size_px/2]
+        px_start = [int(np.round(px_start[0],decimals=0)),int(np.round(px_start[1],decimals=0))]
+        px_end = [int(np.round(px_end[0],decimals=0)),int(np.round(px_end[1],decimals=0))]
+        for i_x in range(px_start[0]-1,px_end[0]+1):
+            for i_y in range(px_start[1]-1,px_end[1]+1):
+                if sqrt((i_x-central_px[0])**2+(i_y-central_px[1])**2) < circle_size_px/2.:
+                    pixel_array.append((i_x,i_y))
+    else:
+        pixel_array.append((central_px[0],central_px[1]))
+    return pixel_array
+
+
+
+def pixel_circle_calculation_px(fitsfile,x,y,r):
+    '''
+    This function returns array of pixels corresponding to circle region with central coordinates Glon, Glat, and radius r
+    x: central pixel x
+    y: central pixel y
+    r: radius of region in arcseconds
+    '''
+    header = fits.getheader(fitsfile)
+    w = WCS(fitsfile)
+    delta = abs(header['CDELT1']) #in degree
+    central_px = [x,y]
     if r is not 'single':
         circle_size_px = 2*r/3600. / delta
         circle_size_px = int(round(circle_size_px))
@@ -61,6 +89,5 @@ def calculate_spectrum(fitsfile,pixel_array):
             spectrum_add = spectrum_add + spectrum_i
     spectrum_average = spectrum_add / (len(pixel_array)-n)
     return spectrum_average
-
 
 
