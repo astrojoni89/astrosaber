5e0ed9ee91dbc32d613bc667b1e1592611e174fe
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-03-03 14:56:27 +0100

    Update plotting.py

--- astroSABER/plotting.py
+++ astroSABER/plotting.py
@@ -39,7 +39,7 @@ def get_figure_params(n_spectra, cols, rowsize, rowbreak):
 
 
 
-def plot_spectra(fitsfiles, coordinates=None, radius=None, path_to_plots=None, cols=3, rowsize=7.75, rowbreak=50, dpi=50, velocity_range=[-110,163]):
+def plot_spectra(fitsfiles, coordinates=None, radius=None, path_to_plots=None, n_spectra=9, cols=3, rowsize=7.75, rowbreak=50, dpi=50, velocity_range=[-110,163]):
     '''
     fitsfiles: list of fitsfiles to plot spectra from
     coordinates: array of central coordinates [[Glon, Glat]] to plot spectra from
@@ -80,7 +80,6 @@ def plot_spectra(fitsfiles, coordinates=None, radius=None, path_to_plots=None, c
         edge = 20
         xsize = fits.getdata(fitsfiles[0]).shape[2]
         ysize = fits.getdata(fitsfiles[0]).shape[1]
-        n_spectra = 9
         cols, rows, rowbreak, colsize = get_figure_params(n_spectra, cols, rowsize, rowbreak)
         figsize = (cols*colsize, rowbreak*rowsize)
         fig = plt.figure(figsize=figsize)
