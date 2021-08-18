dd49829c87f4e68edbfda350da57e85ed5418e24
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-08-18 14:27:43 +0200

    Update hisa_asymmetric_least_squares_fit_example.py
    This is a script

--- example/hisa_asymmetric_least_squares_fit_example.py
+++ example/hisa_asymmetric_least_squares_fit_example.py
@@ -6,43 +6,42 @@ from astropy import units as u
 from astroSABER.hisa import HisaExtraction
 from astroSABER.plotting import plot_spectra
 
+def main():
+    ###HI data to extract HISA
+    image_HI = 'HI_THOR_test_cube.fits'
 
-###HI data to extract HISA
-image_HI = 'HI_THOR_test_cube.fits'
+    ###initialize hisa extraction
+    hisa = HisaExtraction(fitsfile=image_HI)
 
+    ###path to noise map (or universal noise value)
+    #hisa.path_to_noise_map = os.path.join('.', 'dir', 'sub', '*.fits')
+    hisa.noise = 4. #Kelvin
 
-###initialize hisa extraction
-hisa = HisaExtraction(fitsfile=image_HI)
+    ###asymmetric least squares smoothing (Eilers et al. 2005) parameters
+    hisa.lam1 = 0.50
+    hisa.p1 = 0.90
+    hisa.lam2 = 0.50
+    hisa.p2 = 0.90
 
-###path to noise map (or universal noise value)
-#hisa.path_to_noise_map = os.path.join('.', 'dir', 'sub', '*.fits')
-hisa.noise = 4. #Kelvin
+    ###maximum number of iterations (this limit is usually reached for strong continuum sources)
+    hisa.niters = 20
 
+    ###this runs the hisa extraction routine
+    hisa.saber()
 
-###asymmetric least squares smoothing (Eilers et al. 2005) parameters
-hisa.lam1 = 0.50
-hisa.p1 = 0.90
-hisa.lam2 = 0.50
-hisa.p2 = 0.90
+    '''
+    the output will be four (three if output_flags = False) files:
+    hisa background spectrum (.fits)
+    hisa spectrum (.fits)
+    number of iterations needed (.fits); good to check for contamination by continuum or noisy pixels
+    (optional) map of flags (.fits); 1: good pixels, 0: flagged spectra that did not meet convergence criteria or were discarded due to missing signal
+    '''
 
+    ###plot nine example spectra at random positions; or give it some coordinates as array; by default the spectra are averaged over one beam size
+    coords = np.loadtxt('coords.txt') #to plot spectra at these positions: plot_spectra(coordinates=coords)
+    fitsfiles = ['HI_THOR_test_cube.fits', 'HI_THOR_test_cube_aslsq_bg_spectrum.fits', 'HI_THOR_test_cube_HISA_spectrum.fits']
+    plot_spectra(fitsfiles, outfile='spectra_astroSABER.pdf', coordinates=None, n_spectra=9, velocity_range=[-110,163])
 
-###maximum number of iterations (this limit is usually reached for strong continuum sources)
-hisa.niters = 20
-
-
-###this runs the hisa extraction routine
-hisa.saber()
-
-'''
-the output will be three (four if output_flags = True) files:
-hisa background spectrum (.fits)
-hisa spectrum (.fits)
-number of iterations needed (.fits); good to check for contamination by continuum or noisy pixels
-(optional) map of flags (.fits); 1: good pixels, 0: flagged spectra that did not meet convergence criteria or were discarded due to missing signal
-'''
-
-
-###plot nine example spectra at random positions; or give it some coordinates as array; by default the spectra are averaged over one beam size
-coords = np.loadtxt('coords.txt') #to plot spectra at these positions: plot_spectra(coordinates=coords)
-fitsfiles = ['HI_THOR_test_cube.fits', 'HI_THOR_test_cube_aslsq_bg_spectrum.fits', 'HI_THOR_test_cube_HISA_spectrum.fits']
-plot_spectra(fitsfiles, outfile='spectra_astroSABER.pdf', coordinates=None, n_spectra=9, velocity_range=[-110,163])
+    
+if __name__ == '__main__':
+    main()
