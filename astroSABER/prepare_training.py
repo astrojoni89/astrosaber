78a97a0dc08ef8f87f4b885eeb04f7f71f9dbd18
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-11-17 13:47:14 +0100

    Update prepare_training.py

--- astroSABER/prepare_training.py
+++ astroSABER/prepare_training.py
@@ -12,7 +12,7 @@ from .utils.quality_checks import goodness_of_fit, get_max_consecutive_channels,
 from .utils.aslsq_helper import velocity_axes, count_ones_in_row, md_header_2d, check_signal_ranges, IterationWarning, say, format_warning
 from .utils.aslsq_fit import baseline_als_optimized
 
-from parallel_processing import init, parallel_process, parallel_process_wo_bar, func, func_wo_bar
+from .parallel_processing import init, parallel_process, parallel_process_wo_bar, func, func_wo_bar
 
 warnings.showwarning = format_warning
 
