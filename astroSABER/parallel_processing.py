57593eb504a30786aadfcdb94f53dd2782108403
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-11-22 13:53:32 +0100

    Update parallel_processing.py

--- astroSABER/parallel_processing.py
+++ astroSABER/parallel_processing.py
@@ -9,9 +9,14 @@ from .utils.quality_checks import goodness_of_fit, get_max_consecutive_channels,
 from tqdm import trange, tqdm
 
 
-def init(data):
-    global ilist
-    ilist = np.arange(len(data))
+#def init(data):
+#    global ilist
+#    ilist = np.arange(len(data))
+    
+def init(mp_info):
+    global mp_ilist, mp_data, mp_params
+    mp_data, mp_params = mp_info
+    mp_ilist = np.arange(len(mp_data))
     
     
 def single_cost_i(i):
