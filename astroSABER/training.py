ac4aa6a469335a44a4eb14ef36da402330b74b0f
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-11-22 13:46:41 +0100

    Update training.py

--- astroSABER/training.py
+++ astroSABER/training.py
@@ -14,8 +14,6 @@ from .utils.quality_checks import goodness_of_fit, get_max_consecutive_channels,
 from .utils.aslsq_helper import velocity_axes, count_ones_in_row, check_signal_ranges, IterationWarning, say, format_warning
 from .utils.aslsq_fit import baseline_als_optimized, two_step_extraction
 
-from .parallel_processing import init, parallel_process, parallel_process_wo_bar, func, func_wo_bar
-
 warnings.showwarning = format_warning
 
 
@@ -110,7 +108,8 @@ class saberTraining(object):
    
         #global lam1_updt, lam2_updt
         self.lam1_updt, self.lam2_updt = lam1, lam2
-        init(self.training_data)
+        import astroSABER.parallel_processing
+        astroSABER.parallel_processing.init(self.training_data)
         results_list = np.array(func_wo_bar(use_ncpus=ncpus, function='cost'))
    
         if get_all:
