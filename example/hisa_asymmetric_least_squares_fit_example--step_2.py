bf3c2f1d2bb96b0ea7d6b6f642795cd4bb1e5487
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2021-11-23 12:24:49 +0100

    Update hisa_asymmetric_least_squares_fit_example--step_2.py

--- example/hisa_asymmetric_least_squares_fit_example--step_2.py
+++ example/hisa_asymmetric_least_squares_fit_example--step_2.py
@@ -1,1 +1,38 @@
+import os
+import numpy as np
+from astropy.io import fits
+from astropy import units as u
 
+from astroSABER.training import saberTraining
+
+
+###step 2: use training and test data obtained in step 1 to find optimal smoothing parameters
+
+
+def main():
+    ###data to use as training and test data
+    filename = 'astrosaber_training/.pickle'
+
+    ###initialize optimization routine
+    train = saberTraining(pickle_file=filename)
+
+    ###you can adjust the number of cpus to use
+    train.ncpus = 4
+
+    ###set the initial guesses for the smoothing parameters (better to start low rather than high)
+    train.lam1_initial = 2.00
+    train.lam2_initial = 1.00
+
+    ###you can always print the prep object and the keyword arguments you can adjust
+    print(train)
+
+    ###this runs the optimization routine
+    train.training()
+
+    '''
+    the output will be one .txt file containing the two optimal smoothing parameters
+    '''
+
+
+if __name__ == '__main__':
+    main()
