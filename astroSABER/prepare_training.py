d3f6967417bb2f182900dcb69b4db6f5318cad9c
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2022-01-20 17:43:43 +0100

    Update prepare_training.py

--- astroSABER/prepare_training.py
+++ astroSABER/prepare_training.py
@@ -182,7 +182,7 @@ class saberPrepare(object):
         self.mock_data['header'] = self.header
 
         self.save_data()
-        plot_pickle_spectra(self.path_to_file, outfile=None, ranges=None, path_to_plots='astrosaber_training/plots', n_spectra=20, rowsize=4., rowbreak=10, dpi=72, velocity_range=[-110,163], vel_unit=u.km/u.s, seed=111)
+        plot_pickle_spectra(self.path_to_file, outfile=None, ranges=None, path_to_plots='astrosaber_training/plots', n_spectra=20, rowsize=4., rowbreak=10, dpi=72, velocity_range=[-110,163], vel_unit=u.km/u.s, seed=self.seed)
 
     def two_step_extraction(self, i):
         flag = 1.
