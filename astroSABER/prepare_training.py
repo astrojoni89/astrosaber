016aeae37fc90e025d73ef8c9aa250b060a894f9
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2022-01-21 14:32:16 +0100

    Update prepare_training.py

--- astroSABER/prepare_training.py
+++ astroSABER/prepare_training.py
@@ -188,7 +188,7 @@ class saberPrepare(object):
             consecutive_channels_hisa = np.array(consecutive_channels_hisa_list).astype(int)
             ranges_hisa = np.array(ranges_hisa_list).astype(int)
             sort_indices = np.argsort(ranges_hisa[:, 0])
-            ranges_hisa = ranges_hisa[sort_indices]
+            ranges_hisa = ranges_hisa[sort_indices].reshape(-1,2)
             mask_ranges_hisa = ranges_hisa[np.where(consecutive_channels_hisa>=0)]
             mask_hisa = mask_channels(self.v, mask_ranges_hisa, pad_channels=2, remove_intervals=None)
             ###
