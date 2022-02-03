5e89814d1e0d5f2e79cc1ae3972fed24f4a794a1
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2022-02-03 19:19:38 +0100

    Update training.py

--- astroSABER/training.py
+++ astroSABER/training.py
@@ -319,11 +319,11 @@ class saberTraining(object):
 
         # Default settings for hyper parameters; these seem to be the most robust hyperparams
         if self.learning_rate_l1 is None:
-            self.learning_rate_l1 = 0.2
+            self.learning_rate_l1 = 0.5
         if self.learning_rate_l2 is None:
-            self.learning_rate_l2 = 0.1
+            self.learning_rate_l2 = 0.5
         if self.eps_l1 is None:
-            self.eps_l1 = 0.05
+            self.eps_l1 = 0.10
         if self.eps_l2 is None:
             self.eps_l2 = 0.10
         if self.window_size is None:
