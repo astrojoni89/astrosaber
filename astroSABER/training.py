5ddb6df3389233598a5050eccb3117c724515054
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2022-03-17 13:13:40 +0100

    Update training.py
    Bugfix to look for signal ranges in test data (else it could conflict with hisa signal ranges)

--- astroSABER/training.py
+++ astroSABER/training.py
@@ -142,7 +142,7 @@ class saberTraining(object):
         ###TODO
         try:
             mask_hisa = self.hisa_mask[i]
-            consecutive_channels, ranges = determine_peaks(self.training_data[i], peak='both', amp_threshold=None)
+            consecutive_channels, ranges = determine_peaks(self.test_data[i], peak='both', amp_threshold=None)
             mask_ranges = ranges[np.where(consecutive_channels>=self.max_consec_ch)]
             mask = mask_channels(self.v, mask_ranges, pad_channels=2, remove_intervals=None)
             ###
@@ -222,7 +222,7 @@ class saberTraining(object):
         ###TODO
         try:
             mask_hisa = self.hisa_mask[i]
-            consecutive_channels, ranges = determine_peaks(self.training_data[i], peak='both', amp_threshold=None)
+            consecutive_channels, ranges = determine_peaks(self.test_data[i], peak='both', amp_threshold=None)
             mask_ranges = ranges[np.where(consecutive_channels>=self.max_consec_ch)]
             mask = mask_channels(self.v, mask_ranges, pad_channels=2, remove_intervals=None)
             ###
