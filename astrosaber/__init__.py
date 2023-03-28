0f0f914362379475b0d52f3f50aafba8025ac1f3
Jonas Syed <75168462+astrojoni89@users.noreply.github.com>
2023-03-28 17:37:42 +0200

    Create __init__.py

---
+++ astrosaber/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+
+__minimum_python_version__ = "3.6"
+
+class UnsupportedPythonError(Exception):
+    pass
+
+if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
+    raise UnsupportedPythonError("astroSABER does not support Python < {}".format(__minimum_python_version__))
