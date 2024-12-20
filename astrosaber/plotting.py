Bugfix velocity units in fits

astrosaber/plotting.py
@@ -134,7 +134,7 @@ def get_title_string(idx, rchi2):
         idx, rchi2_string)
     return title
 
-def plot_spectra(fitsfiles, outfile='spectra.pdf', coordinates=None, radius=None, path_to_plots='.', n_spectra=9, rowsize=4., rowbreak=10, dpi=72, velocity_range=[-110,163], vel_unit=u.km/u.s, seed=111):
+def plot_spectra(fitsfiles, outfile='spectra.pdf', coordinates=None, radius=None, path_to_plots='.', n_spectra=9, rowsize=4., rowbreak=10, dpi=72, velocity_range=[-110,163], cunit3='m/s', vel_unit=u.km/u.s, seed=111):
     """
     fitsfiles: list of fitsfiles to plot spectra from
     coordinates: array of central coordinates [[Glon, Glat]] to plot spectra from
@@ -159,7 +159,7 @@ def plot_spectra(fitsfiles, outfile='spectra.pdf', coordinates=None, radius=None
                     pixel_array = pixel_circle_calculation(fitsfile,glon=coordinates[i,0],glat=coordinates[i,1],r=radius)
                     spectrum = calculate_spectrum(fitsfile,pixel_array)
                     header = fits.getheader(fitsfile)
-                    velocity = velocity_axes(fitsfile)
+                    velocity = velocity_axes(fitsfile, cunit3=cunit3)
                     velo_min, velo_max = find_nearest(velocity,np.amin(velocity_range)), find_nearest(velocity,np.amax(velocity_range))
                     ax.plot(velocity[velo_min:velo_max], spectrum[velo_min:velo_max], drawstyle=draw_list[idx], color=color_list[idx], linestyle=line_list[idx])
                 add_figure_properties(ax, header=header, fontsize=fontsize, velocity_range=velocity_range, vel_unit=vel_unit)
@@ -175,7 +175,7 @@ def plot_spectra(fitsfiles, outfile='spectra.pdf', coordinates=None, radius=None
                     radius = 1/2. * (beam*3600)
                     pixel_array = pixel_circle_calculation(fitsfile,glon=coordinates[i,0],glat=coordinates[i,1],r=radius)
                     spectrum = calculate_spectrum(fitsfile,pixel_array)
-                    velocity = velocity_axes(fitsfile)
+                    velocity = velocity_axes(fitsfile, cunit3=cunit3)
                     velo_min, velo_max = find_nearest(velocity,np.amin(velocity_range)), find_nearest(velocity,np.amax(velocity_range))
                     ax.plot(velocity[velo_min:velo_max], spectrum[velo_min:velo_max], drawstyle=draw_list[idx], color=color_list[idx], linestyle=line_list[idx])
                 add_figure_properties(ax, header=header, fontsize=fontsize, velocity_range=velocity_range, vel_unit=vel_unit)
@@ -204,7 +204,7 @@ def plot_spectra(fitsfiles, outfile='spectra.pdf', coordinates=None, radius=None
                     pixel_array = pixel_circle_calculation_px(fitsfile,x=xValue,y=yValue,r=radius)
                     spectrum = calculate_spectrum(fitsfile,pixel_array)
                     header = fits.getheader(fitsfile)
-                    velocity = velocity_axes(fitsfile)
+                    velocity = velocity_axes(fitsfile, cunit3=cunit3)
                     velo_min, velo_max = find_nearest(velocity,np.amin(velocity_range)), find_nearest(velocity,np.amax(velocity_range))
                     ax.plot(velocity[velo_min:velo_max], spectrum[velo_min:velo_max], drawstyle=draw_list[idx], color=color_list[idx], linestyle=line_list[idx])
                 add_figure_properties(ax, header=header, fontsize=fontsize, velocity_range=velocity_range, vel_unit=vel_unit)
@@ -227,7 +227,7 @@ def plot_spectra(fitsfiles, outfile='spectra.pdf', coordinates=None, radius=None
                     radius = 1/2. * (beam*3600)
                     pixel_array = pixel_circle_calculation_px(fitsfile,x=xValue,y=yValue,r=radius)
                     spectrum = calculate_spectrum(fitsfile,pixel_array)
-                    velocity = velocity_axes(fitsfile)
+                    velocity = velocity_axes(fitsfile, cunit3=cunit3)
                     velo_min, velo_max = find_nearest(velocity,np.amin(velocity_range)), find_nearest(velocity,np.amax(velocity_range))
                     ax.plot(velocity[velo_min:velo_max], spectrum[velo_min:velo_max], drawstyle=draw_list[idx], color=color_list[idx], linestyle=line_list[idx])
                 add_figure_properties(ax, header=header, fontsize=fontsize, velocity_range=velocity_range, vel_unit=vel_unit)

astrosaber/utils/aslsq_helper.py
@@ -30,14 +30,17 @@ def find_nearest(array: np.ndarray, value: float) -> int:
     return idx
 
 
-def velocity_axes(name: Path) -> np.ndarray:
+def velocity_axes(name: Path, cunit3: str = 'm/s') -> np.ndarray:
     """
-    Get velocity axis from FITS file.
+    Get velocity axis from FITS file in units of km/s.
 
     Parameters
     ----------
     name : Path
         Path to FITS file to get velocity axis from.
+    cunit3 : str, optional
+        Type of velocity unit specified in the fits file header keyword 'CUNIT3'.
+        Default is 'm/s'.
 
     Returns
     -------
@@ -47,7 +50,12 @@ def velocity_axes(name: Path) -> np.ndarray:
     header = fits.getheader(name)
     n = header['NAXIS3']
     velocity = (header['CRVAL3'] - header['CRPIX3'] * header['CDELT3']) + (np.arange(n)+1) * header['CDELT3']
-    velocity = velocity / 1000
+    if cunit3 == 'm/s':
+        velocity = velocity / 1000
+    elif cunit3 == 'km/s':
+        return velocity
+    else:
+        raise ValueError('Unknown velocity unit (cunit3)')
     return velocity
 
 